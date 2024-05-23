03bf779a32e3d652737ca89ae13494057232b41d
Gabriel Lourenço <gabriels211@hotmail.com>
2024-05-23 09:38:06 -0300

    feat: implementa logica do game e design

--- gabo-app/src/App.tsx
+++ gabo-app/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Player = "O" | "X";
+
 function App() {
+  const [turn, setTurn] = useState("O");
+  const [winner, setWinner] = useState<Player | null>(null)
+  const [draw, setDraw] = useState<boolean | null>(null)
+  
+  {
+
+  }
+
+  const [marks, setMarks] = useState({})
+  
+  const getSquares = () =>{
+    return new Array(9).fill(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>O ganhou</h1>
+      <h1>Empate</h1>
+      <button>Jogar novamente</button>
+      <p>É a vez de X</p>
+      <div className="board">
+      {getSquares().map((_, i) =>(
+        <div className="cell">X</div>
+      ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
